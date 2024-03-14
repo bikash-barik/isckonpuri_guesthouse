ba60a45c8e4e4f9d3847b54a528d26177e0692e8
sauravsable <sauravsable4102@gmail.com>
2024-03-14 13:07:24 +0530

    backend link added

--- src/components/BookNowForm.jsx
+++ src/components/BookNowForm.jsx
@@ -1,34 +1,164 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './booknowform.css';
 // import { IoMdClose } from "react-icons/io";
-
+import axios from "axios";
+import loadingImage from '../assets/hotelbookingreloader.gif';
 export default function BookNowForm() {
+
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState({});
+
+  const [Name,setName] = useState('');
+  const [phoneNumber,setPhoneNumber] = useState('');
+  const [email,setEmail] = useState("");
+  const [address,setAddress] = useState("");
+  const [service,setService] = useState([]);
+
+  const handleCheckboxChange = (e) => {
+    const { id, checked } = e.target;
+    setService((prevService) =>
+      checked ? [...prevService, id] : prevService.filter((item) => item !== id)
+    );
+
+    console.log(service);
+  };
+
+  const [errors, setErrors] = useState({});
+
+  const validation = () => {
+    const newErrors = {};
+    const emailPattern = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    const phonePattern = /^[\d+\-\s()]*$/;
+
+
+    if (!Name) {
+      newErrors.Name = 'First Name is required';
+    }
+
+    if (!phoneNumber) {
+      newErrors.phoneNumber = 'Mobile Number is required';
+    } else if (!phonePattern.test(phoneNumber)) {
+      newErrors.phoneNumber = 'Please enter correct Correct Mobile Number';
+    }
+
+    if (!email) {
+      newErrors.email = 'Email is required';
+    } else if (!emailPattern.test(email)) {
+      newErrors.email = 'Please enter your Institute Email ID';
+    }
+
+    if (!address) {
+      newErrors.address = 'Address is required';
+    }
+
+    if (service.length === 0) {
+      newErrors.service = 'Services is required';
+    }
+   
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+  
+  const submitCheckoutForm = async (e) =>{
+    e.preventDefault();
+
+    const formData = {
+      Name,
+      phoneNumber,
+      email,
+      address,
+      service,
+    };
+
+    if(validation()){
+      try {
+        setLoading(true);
+        setMessage({text:"Wait Until Your Data is Submitted!..."})
+
+        await axios.post("https://iskconpuriguesthouse-backend.onrender.com/BookNowForm", formData);
+        // await fetch("http://localhost:5500/BookNowForm", {method: "POST", body: formData});
+        setName("");
+        setEmail("");
+        setPhoneNumber("")
+        setAddress("");
+        setService([]);
+        setMessage({ type: 'success', text: 'Data submitted successfully!' });
+        setTimeout(() => {
+          setLoading(false);
+        }, 5000);
+      } catch (error) {
+        setMessage({ type: 'error', text: error });
+        setTimeout(() => {
+         setLoading(false);
+       }, 5000);
+      }
+    }
+    else{
+      alert("All the fields are required...")
+    }
+  }
+  
   return (
-    <section className="booknowection">    
+    <>
+    {
+      loading ?
+      <section
+      style={{
+        height: "90vh",
+        display: "flex",
+        flexDirection:"column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src={loadingImage}
+        alt="Loading"
+        className="loading-image"
+        style={{ width: "300px",height:"300px" }}
+      />
+      <div className="loadingimagemessage">
+          <h2 style={{ color: message.type === 'success' ? 'green' : message.type === 'error' ? 'red' :'yellow'}}>{message.text}</h2>
+          </div>
+    </section>
+    :
+    <section className="booknowection">
+     
       <div className="booknowrow1">
       <h1 className='booknowheading'>Book Now Form</h1>
       </div>
       <div className="booknowrow">
     
          <div className="booknowcolumninnerdiv">
-                <input type="text" className='booknowinput' placeholder='Name'/>
-                <input type="text" className='booknowinput' placeholder='Contact Number'/>
-                <input type="email" className='booknowinput' placeholder='Email'/>
-                <input type="text" className='booknowinput' placeholder='Address'/>
+                <input type="text" className='booknowinput' placeholder='Name'  value={Name} required onChange={(e)=>{setName(e.target.value)}}/>
+                {errors.Name && (<span className="checkoutspan">{errors.Name}</span>)}
+
+                <input type="text" className='booknowinput' placeholder='Contact Number'  value={phoneNumber} onChange={(e)=>{setPhoneNumber(e.target.value)}}/>
+                {errors.phoneNumber && (<span className="checkoutspan">{errors.phoneNumber}</span>)}
+
+                <input type="email" className='booknowinput' placeholder='Email'  value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                {errors.email && (<span className="checkoutspan">{errors.email}</span>)}
+
+                <input type="text" className='booknowinput' placeholder='Address'  value={address} onChange={(e)=>{setAddress(e.target.value)}}/>
+                {errors.address && (<span className="checkoutspan">{errors.address}</span>)}
          </div>
                 
           <div className="booknowcolumninnerdiv flex flex-col">
-               <label htmlFor="" className='booknowlabel'> <input type="checkbox" /> Hotel Booking</label>
-               <label htmlFor="" className='booknowlabel'> <input type="checkbox" /> Cab Booking</label>
-               <label htmlFor="" className='booknowlabel'> <input type="checkbox" /> Prsadam Booking</label>
-               <label htmlFor="" className='booknowlabel'> <input type="checkbox" /> Tour </label>
-               <label htmlFor="" className='booknowlabel'> <input type="checkbox" /> Tourist Guide </label>
+               <label htmlFor="" className='booknowlabel'> <input type="checkbox"  id="HotelBooking" onChange={handleCheckboxChange} /> Hotel Booking</label>
+               <label htmlFor="" className='booknowlabel'> <input type="checkbox" id="CabBooking" onChange={handleCheckboxChange} /> Cab Booking</label>
+               <label htmlFor="" className='booknowlabel'> <input type="checkbox" id="PrasadhBooking" onChange={handleCheckboxChange} /> Prsadam Booking</label>
+               <label htmlFor="" className='booknowlabel'> <input type="checkbox" id="Tour" onChange={handleCheckboxChange} /> Tour </label>
+               <label htmlFor="" className='booknowlabel'> <input type="checkbox" id="TouristGuide" onChange={handleCheckboxChange} /> Tourist Guide </label>
           </div>
+          {errors.service && (<span className="checkoutspan">{errors.service}</span>)}   
 
           <div className="booknowcolumninnerdiv mt-4">
-               <input type="submit" className='booknowinputbtn' placeholder='SEND MESSAGE'/>
+               <button className='booknowinputbtn' onClick={submitCheckoutForm}>SEND MESSAGE</button>
           </div>
       </div>
-    </section>
+
+</section> 
+}   
+</>
   )
 }
--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { FaPhoneFlip } from "react-icons/fa6";
 import { MdMarkEmailUnread } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
@@ -6,11 +6,120 @@ import { phoneNumber } from "../Textfiles/webiste-Content";
 import { email } from "../Textfiles/webiste-Content";
 import { address } from "../Textfiles/webiste-Content";
 import '../components/contactus.css';
+import axios from 'axios';
+import loadingImage from '../assets/hotelbookingreloader.gif';
 
 const ContactUsPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState({});
+  
+  const [firstName,setfirstName] = useState('');
+  const [lastName,setlastName] = useState('');
+  const [emailAddress,setEmailAddress] = useState("");
+  const [contactNumber,setContactNumber] = useState('');
+  const [contactMessage,setContactMessage] = useState('');
+
+  const [errors, setErrors] = useState({});
+
+  const validation = () => {
+    const newErrors = {};
+    const emailPattern = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    const phonePattern = /^[\d+\-\s()]*$/;
+
+
+    if (!firstName) {
+      newErrors.firstName = 'First Name is required';
+    }
+
+    if (!lastName) {
+      newErrors.lastName = 'Last Name is required';
+    }
+
+
+    if (!contactNumber) {
+      newErrors.contactNumber = 'Mobile Number is required';
+    } else if (!phonePattern.test(contactNumber)) {
+      newErrors.contactNumber = 'Please enter correct Correct Mobile Number';
+    }
+
+    if (!emailAddress) {
+      newErrors.emailAddress = 'Email is required';
+    } else if (!emailPattern.test(emailAddress)) {
+      newErrors.emailAddress = 'Please enter your Institute Email ID';
+    }
+
+    if (!contactMessage) {
+      newErrors.contactMessage = 'Message is required';
+    }
+
+   
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+  
+  const submitContactUsForm = async (e) =>{
+    e.preventDefault();
+
+    const formData = {
+      firstName,
+      lastName,
+      contactNumber,
+      emailAddress,
+      contactMessage,
+    };
+
+    if(validation()){
+      try {
+        setLoading(true);
+        setMessage({text:"Wait Until Your Data is Submitted!..."})
+
+        await axios.post("https://iskconpuriguesthouse-backend.onrender.com/ContactUsForm", formData);
+        // await fetch("http://localhost:5500/BookNowForm", {method: "POST", body: formData});
+        setfirstName("");
+        setlastName("");
+        setEmailAddress("")
+        setContactNumber("");
+        setContactMessage('');
+        setMessage({ type: 'success', text: 'Data submitted successfully!' });
+        setTimeout(() => {
+          setLoading(false);
+        }, 5000);
+      } catch (error) {
+        setMessage({ type: 'error', text: error });
+        setTimeout(() => {
+         setLoading(false);
+       }, 5000);
+      }
+    }
+    else{
+      alert("All the fields are required...")
+    }
+  }
 
   return (
     <>
+     {
+      loading ?
+      <section
+      style={{
+        height: "90vh",
+        display: "flex",
+        flexDirection:"column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src={loadingImage}
+        alt="Loading"
+        className="loading-image"
+        style={{ width: "300px",height:"300px" }}
+      />
+      <div className="loadingimagemessage">
+          <h2 style={{ color: message.type === 'success' ? 'green' : message.type === 'error' ? 'red' :'yellow'}}>{message.text}</h2>
+          </div>
+    </section>
+    :
     <section className='contactussection pt-32'>
       <div className="contactusrow1">
       <h1 className='contactusheading'>Contact Us</h1>
@@ -20,23 +129,24 @@ const ContactUsPage = () => {
         <div className="contactuscolumn1">
           <h1 className='contactuscolumnheading'>Send us a Message</h1>
           <div className="contactuscolumninnerdiv ">
-                <input type="text" className='contactusinput mr-3' placeholder='First Name'/>
-                <input type="text" className='contactusinput' placeholder='Last Name'/>
+                <input type="text" className='contactusinput mr-3' placeholder='First Name'  value={firstName} required onChange={(e)=>{setfirstName(e.target.value)}}/>
+                {/* {errors.firstName && (<span className="checkoutspan">{errors.firstName}</span>)} */}
+                <input type="text" className='contactusinput' placeholder='Last Name'  value={lastName} required onChange={(e)=>{setlastName(e.target.value)}}/>
           </div>
 
           <div className="contactuscolumninnerdiv">
-                <input type="email" className='contactusinput mr-3' placeholder='Email'/>
-                <input type="text" className='contactusinput' placeholder='Contact Number'/>
+                <input type="email" className='contactusinput mr-3' placeholder='Email'  value={emailAddress} required onChange={(e)=>{setEmailAddress(e.target.value)}}/>
+                <input type="text" className='contactusinput' placeholder='Contact Number'  value={contactNumber} required onChange={(e)=>{setContactNumber(e.target.value)}}/>
           </div>
 
           <div className="contactuscolumninnerdiv">
-                <textarea name="" className='contactusinput' id="" cols="50" rows="10" placeholder='Message'>
+                <textarea name="" className='contactusinput' id="" cols="50" rows="10" placeholder='Message'  value={contactMessage} required onChange={(e)=>{setContactMessage(e.target.value)}}>
                   Message
                 </textarea>
           </div>
 
           <div className="contactuscolumninnerdiv flex justify-end">
-               <input type="submit" className='contactusinputbtn' placeholder='SEND MESSAGE'/>
+               <button className='contactusinputbtn' onClick={submitContactUsForm}> SEND MESSAGE </button>
           </div>
 
         </div>
@@ -95,6 +205,7 @@ const ContactUsPage = () => {
         </div>
       </div>
     </section>
+}
     </>
   );
 };
