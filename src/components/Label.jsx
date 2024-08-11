f7cf8e1245c68027552266d3e523fbab911252b1
bikash-barik <bikashbarik16623@gmail.com>
2024-08-11 06:29:21 +0530

    COmmit For mobile number  changes

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -6,7 +6,8 @@ const Label = () => {
     <div className="label">
       Please check the travel advisory issued by the concerned state
       government/local authorities before booking, as some places may have
-      COVID-19 related travel/lodging restrictions.
+      COVID-19 related travel/lodging restrictions. <br/>
+      Note -  ISKCON Puri does not have guest house. So we arrange accomodation with help of other guest houses at Puri
     </div>
   );
 };
